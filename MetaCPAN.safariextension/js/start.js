a2422404c5a707ad9b3db6b2880b8639b5860388
Paul M. Lambert <plambert@plambert.net>
2017-12-23 11:00:22 -0800

    do not redirect rt.cpan.org URLs

--- MetaCPAN.safariextension/js/start.js
+++ MetaCPAN.safariextension/js/start.js
@@ -1,9 +1,11 @@
 // javascript injected at page start
 
 var loc=window.location + "";
-var newloc=loc.replace(/^(https?:\/\/[a-z0-9\-\.]*)\.cpan\.org/, '$1.mcpan.org');
-if (loc != newloc) {
-  // alert(JSON.stringify({"loc":loc,"new":newloc}));
-  window.location=newloc;
+if (! loc.match(/^https?:\/\/(rt)\.cpan\.org/) {
+  var newloc=loc.replace(/^(https?:\/\/[a-z0-9\-\.]*)\.cpan\.org/, '$1.mcpan.org');
+  if (loc != newloc) {
+    // alert(JSON.stringify({"loc":loc,"new":newloc}));
+    window.location=newloc;
+  }
 }
 
